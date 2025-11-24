8bd8f8daf12e0af907eda22dbd611567e8b16bb1
Anthony Avila <anthonyavila422@gmail.com>
2025-11-23 23:01:42 -0800

    file reader first function

--- Capstone C++ 2 2025/FileReader.cpp
+++ Capstone C++ 2 2025/FileReader.cpp
@@ -8,15 +8,53 @@
 */
 
 #include "FileReader.h"
+#include "BotType.h"
+#include "BotList.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 void readBotData(BotList& botList)
 {
+    ifstream botDataFile("bots.csv");
+
+    if (botDataFile.fail())
+    {
+        cout << "Error opening file." << endl;
+    }
+    else
+    {
+        string line;
+        getline(botDataFile, line);
+
+        while (getline(botDataFile, line))
+        {
+            stringstream lineStream(line);
+
+            string inName, inAbility, inTrait;
+
+            getline(lineStream, inName, ',');
+            getline(lineStream, inAbility, ',');
+            getline(lineStream, inTrait, ',');
+
+            BotType inputBot(inName, inAbility, inTrait);
+            botList.insertBot(inputBot);
+        }
+        botDataFile.close();
+    }
 
 }
 
 void readElectionResults(Election& electionResults)
 {
+    ifstream votesDataFile("votes.csv");
 
+    if (votesDataFile.fail())
+    {
+        cout << "Error opening file." << endl;
+    }
 }
